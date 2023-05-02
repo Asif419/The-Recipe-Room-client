d727def6d27009e8988a9d02ec0ae31dd56830ae
Asif419 <asshah419@gmail.com>
2023-05-02 11:33:48 +0600

    NavLink added with active class

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProviders';
+import Header from '../pages/Shared/Header';
+import Footer from '../pages/Shared/Footer';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
   // const { user } = useContext(AuthContext);
   return (
     <>
-      <h2>hello bd</h2>
+      <div className='container mx-auto'>
+        <Header></Header>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </>
   );
 };
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,11 +2,29 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+import Recipes from "../pages/Home/Recipes";
+import Blogs from "../pages/Home/Blogs";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/recipes',
+        element: <Recipes></Recipes>
+      },
+      {
+        path: '/blogs',
+        element: <Blogs></Blogs> 
+      },
+    ]
   },
 ]);
 
