1b13fbc0fd6143663df03844aa84ae4a888af5e6
Asif419 <asshah419@gmail.com>
2023-05-02 10:31:34 +0600

    router, context API, firebase, components added

---
+++ src/layout/Main.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProviders';
+
+const Main = () => {
+  // const { user } = useContext(AuthContext);
+  return (
+    <>
+      <h2>hello bd</h2>
+    </>
+  );
+};
+
+export default Main;
