d04f4a7c04bb784515614dabaeab8073523e0013
Asif419 <asshah419@gmail.com>
2023-05-03 12:49:28 +0600

    recipe card done

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ error and success message in login
 registration location direction
 profile picture dynamic in navbar
 empty login and register page after submit
+bio on chefs description
--- src/pages/Home/ChefDetails/SingleRecipe.jsx
+++ src/pages/Home/ChefDetails/SingleRecipe.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
 const SingleRecipe = ({ recipe }) => {
-  const { recipe_id, recipe_url, recipe_name, ingredients } = recipe;
+  const { recipe_id, recipe_url, recipe_name, ingredients, cooking_method } = recipe;
   return (
     <div className="">
       <div className="card bg-base-100 shadow-xl rounded-lg flex items-stretch">
-        <figure className="h-60"><img className='h-full w-full object-cover' src={recipe.recipe_url} alt="recipe" /></figure>
-        <div className="card-body min-h-[55vh] flex-1">
-          <h2 className="card-title text-2xl text-pink-800">{recipe.recipe_name}</h2>
+        <figure className="h-60"><img className='h-full w-full object-cover' src={recipe_url} alt="recipe" /></figure>
+        <div className="card-body min-h-[550px] flex-1">
+          <h2 className="card-title text-2xl text-pink-800">{recipe_name}</h2>
           <span className='font-bold'>Ingredients</span>
-          <p>{recipe.ingredients}</p>
-          <div className="card-actions justify-end">
+          <div>
+            {ingredients.map((ingredient, index) => {
+              if ((index + 1) === ingredients.length) {
+                return <span> {ingredient}</span>
+              }
+              else
+                return <span> {ingredient},</span>
+            })}
+          </div>
+          <span className='font-bold'>Cooking method</span>
+          <p>{cooking_method}</p>
+          <div className="card-actions justify-end mt-auto">
             <button className="btn btn-secondary">Make Favorite</button>
           </div>
         </div>
       </div>
     </div>
-
-
   );
 };
 
